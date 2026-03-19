1a393b9ed7fa624bd3b53aeb54d2b2ab3af007a9
ciamara <mruktamara64@gmail.com>
2026-03-19 09:42:46 +0100

    swapped average color extraction for most vivid color

--- AudioVisualiser/AudioVisualiser/VisualHelper.cs
+++ AudioVisualiser/AudioVisualiser/VisualHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,7 +36,7 @@ namespace AudioVisualiser
 
         public static async Task<string> ExtractFeatureColor(SoftwareBitmap cover)
         {
-            if (cover == null) return "#963719";    // default
+            //if (cover == null) return "#963719";
 
             return await Task.Run(() =>
             {
@@ -46,39 +47,53 @@ namespace AudioVisualiser
 
                 byte[] pixels = buffer.ToArray();
 
-                long rSum = 0, gSum = 0, bSum = 0;
-                int count = 0;
+                var colorCounts = new Dictionary<Vector3, int>();
+                //colorCounts.Add(new Vector3(150, 55, 25), 1); // default color #963719
+                colorCounts.Add(new Vector3(138, 138, 138), 1);   // default greyscale #8a8a8a
+                int color_tolerance = 40; // color uniquness tolerance, values range 0-440 preferably choose lower numbers
+                int vivid_tolerance = 40; // tolerance for avoiding the grayscale, higher the more vivid the picked colors will be
+
 
                 for (int i = 0; i < pixels.Length; i += 32)
                 {
-                    rSum += pixels[i];     // r
-                    gSum += pixels[i + 1]; // g
-                    bSum += pixels[i + 2]; // b
-                    count++;
+                    byte r_byte = pixels[i];
+                    byte g_byte = pixels[i + 1];
+                    byte b_byte = pixels[i + 2];
+
+                    // looking for the most vivid colors
+                    int max = Math.Max(r_byte, Math.Max(g_byte, b_byte));
+                    int min = Math.Min(r_byte, Math.Min(g_byte, b_byte));
+                    int vividness = max - min;
+
+                    if (vividness < vivid_tolerance)
+                    {
+                        continue; // if not vivid enough, skip
+                    }
+
+                    Vector3 rgb = new Vector3(r_byte, g_byte, b_byte);
+                    bool new_color = true;
+                    foreach (var color in colorCounts.Keys)
+                    {
+                        // check if it is close enough to any color in the dictionary
+                        // if not add it
+                        if (Vector3.Distance(rgb, color) <= color_tolerance)
+                        {
+                            colorCounts[color]++;
+                            new_color = false;
+                            break;
+                        }
+                    }
+                    if (new_color)
+                    {
+                        colorCounts.Add(rgb, 1);
+                    }
                 }
 
-                if (count == 0) return "#963719";
+                Vector3 dominantColor = colorCounts.OrderByDescending(kvp => kvp.Value).First().Key;
 
-                // averages
-                int r = (int)(rSum / count);
-                int g = (int)(gSum / count);
-                int b = (int)(bSum / count);
-
-                double brightness = (0.299 * r) + (0.587 * g) + (0.114 * b);
-                double threshold = 50.0;
-
-                //if (brightness < threshold)
-                //{
-                //    float factor = 0.3f;
-
-                //    r = (int)(r + (255 - r) * factor);
-                //    g = (int)(g + (255 - g) * factor);
-                //    b = (int)(b + (255 - b) * factor);
-                //}
-
-                r = Math.Clamp(r, 0, 255);
-                g = Math.Clamp(g, 0, 255);
-                b = Math.Clamp(b, 0, 255);
+                int r = Math.Clamp((int)dominantColor.X, 0, 255);
+                int g = Math.Clamp((int)dominantColor.Y, 0, 255);
+                int b = Math.Clamp((int)dominantColor.Z, 0, 255);
 
                 return $"#{r:X2}{g:X2}{b:X2}";
             });

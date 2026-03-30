7462a53d026b34f8f323341bf6ddb88d936e9dbf
ciamara <mruktamara64@gmail.com>
2026-03-30 16:56:23 +0200

    checking media support

--- AudioVisualiser/AudioVisualiser/Burner.cs
+++ AudioVisualiser/AudioVisualiser/Burner.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
@@ -19,7 +20,7 @@ namespace AudioHelpers
         /// </summary>
         /// <param name="source">mp3 file directory</param>
         /// <param name="volumeLabel">disc name</param>
-        public static async Task BurnCD(string source, string volumeLabel)
+        public static async Task BurnCD(string source, string volumeLabel, string bootFilePath = null)
         {
             await Task.Run(() =>
             {
@@ -27,9 +28,16 @@ namespace AudioHelpers
                 MsftDiscRecorder2 discRecorder = null;
                 MsftFileSystemImage fileSystemImage = null;
                 MsftDiscFormat2Data discFormatData = null;
+                IMAPI2.IStream bootStream = null;
+
+                [DllImport("shlwapi.dll", CharSet = CharSet.Unicode, ExactSpelling = true, PreserveSig = false)]
+                static extern void SHCreateStreamOnFileW(string fileName, uint mode, out IMAPI2.IStream stream);
+
+                const uint STGM_READ = 0x00000000;
 
                 try
                 {
+                    // checking drive support
                     discMaster = new MsftDiscMaster2();
 
                     if (discMaster.Count == 0)
@@ -43,13 +51,122 @@ namespace AudioHelpers
                     discRecorder = new MsftDiscRecorder2();
                     discRecorder.InitializeDiscRecorder(recorderUniqueId);
 
-
                     Console.WriteLine($"Active disc recorder: {discRecorder.ActiveDiscRecorder}");
 
                     foreach(string mountPoint in discRecorder.VolumePathNames)
                     {
                         Console.WriteLine($"Mount point: {mountPoint}");
                     }
+
+                    // checking media support
+                    discFormatData = new MsftDiscFormat2Data();
+                    discFormatData.Recorder = discRecorder;
+
+                    if (discFormatData.IsRecorderSupported(discRecorder))
+                    {
+                        Console.WriteLine("Current recorder IS supported.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Current recorder IS NOT supported.");
+                    }
+
+                    if (discFormatData.IsCurrentMediaSupported(discRecorder))
+                    {
+                        Console.WriteLine("Current media IS supported.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Current media IS NOT supported.");
+                    }
+
+                    Console.WriteLine($"client name: {discFormatData.ClientName}");
+
+                    IMAPI2.IMAPI_MEDIA_PHYSICAL_TYPE mediaType = discFormatData.CurrentPhysicalMediaType;
+
+                    switch (mediaType)
+                    {
+                        case IMAPI2.IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_UNKNOWN:
+                            Console.WriteLine("Empty device or an unknown disc type.");
+                            break;
+
+                        case IMAPI2.IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_CDROM:
+                            Console.WriteLine("CD-ROM");
+                            break;
+
+                        case IMAPI2.IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_CDR:
+                            Console.WriteLine("CD-R");
+                            break;
+
+                        case IMAPI2.IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_CDRW:
+                            Console.WriteLine("CD-RW");
+                            break;
+
+                        case IMAPI2.IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_DVDROM:
+                            Console.WriteLine("Read-only DVD drive and/or disc");
+                            break;
+
+                        case IMAPI2.IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_DVDRAM:
+                            Console.WriteLine("DVD-RAM");
+                            break;
+
+                        case IMAPI2.IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_DVDPLUSR:
+                            Console.WriteLine("DVD+R");
+                            break;
+
+                        case IMAPI2.IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_DVDPLUSRW:
+                            Console.WriteLine("DVD+RW");
+                            break;
+
+                        case IMAPI2.IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_DVDPLUSR_DUALLAYER:
+                            Console.WriteLine("DVD+R Dual Layer media");
+                            break;
+
+                        case IMAPI2.IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_DVDDASHR:
+                            Console.WriteLine("DVD-R");
+                            break;
+
+                        case IMAPI2.IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_DVDDASHRW:
+                            Console.WriteLine("DVD-RW");
+                            break;
+
+                        case IMAPI2.IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_DVDDASHR_DUALLAYER:
+                            Console.WriteLine("DVD-R Dual Layer media");
+                            break;
+
+                        case IMAPI2.IMAPI_MEDIA_PHYSICAL_TYPE.IMAPI_MEDIA_TYPE_DISK:
+                            Console.WriteLine("Randomly-writable, hardware-defect ");
+                            break;
+                    }
+
+                    //// file system image
+                    //fileSystemImage = new MsftFileSystemImage();
+                    //fileSystemImage.ChooseImageDefaults((IMAPI2FS.IDiscRecorder2)discRecorder);
+                    //fileSystemImage.VolumeName = volumeLabel;
+
+                    //// boot image 
+
+                    //dynamic opts = new BootOptions();
+                    //opts.Manufacturer = "Microsoft";
+                    //opts.PlatformId = PlatformId.PlatformX86;
+                    //opts.Emulation = EmulationType.EmulationNone;
+
+                    //// steam
+                    //SHCreateStreamOnFileW(bootFilePath, STGM_READ, out bootStream);
+
+                    //opts.AssignBootImage(bootStream);
+                    //fileSystemImage.BootImageOptions = opts;
+
+                    //fileSystemImage.Root.AddTree(source, false);
+
+                    //// result image
+                    //IFileSystemImageResult result = fileSystemImage.CreateResultImage();
+                    //IMAPI2.IStream imageStream = (IMAPI2.IStream)result.ImageStream;
+
+                    //// write
+                    //discFormatData.Write(imageStream);
+
+                    //BurnCompleted?.Invoke(null, EventArgs.Empty);
                 }
                 catch (Exception ex)
                 {
